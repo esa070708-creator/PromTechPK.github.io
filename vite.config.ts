332c301edd6ee47e8c150b25ac84eb7b431aff68
Seliverstov Egor <esa070708@gmail.com>
2026-04-17 18:47:27 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
+  // Для твоего случая (репозиторий PromTechPK.github.io):
+  base: '/PromTechPK.github.io/',  // ✅ Если сайт будет по адресу: esa070708-creator.github.io/PromTechPK.github.io/
 })
