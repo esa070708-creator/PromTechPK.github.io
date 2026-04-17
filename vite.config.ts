1a980f58fda23df27cd04dec7ba7451fb3ee0a85
Seliverstov Egor <esa070708@gmail.com>
2026-04-17 19:24:18 +0300

    Деплой на GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
-  // Для твоего случая (репозиторий PromTechPK.github.io):
-  base: '/PromTechPK.github.io/',  // ✅ Если сайт будет по адресу: esa070708-creator.github.io/PromTechPK.github.io/
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  base: '/PromPK/' 
 })
